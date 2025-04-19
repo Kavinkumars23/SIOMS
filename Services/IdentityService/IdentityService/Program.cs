26335f8e5b8b31af93af00220a10168e56f67ecb
Kavinkumar23 <kavinkumarsubramaniam23@gmail.com>
2025-04-19 01:16:35 +0100

    created APPUser ENtity, APPDbContext

--- Services/IdentityService/IdentityService/Program.cs
+++ Services/IdentityService/IdentityService/Program.cs
@@ -1,17 +1,43 @@
+﻿using IdentityService.Domain.Entities;
+using IdentityService.Infrastructure.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Add EF Core + AppDbContext
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Add ASP.NET Identity
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
+// Enable CORS (for frontend)
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+app.UseSwagger();
+app.UseSwaggerUI();
 
+app.UseCors("AllowFrontend");
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
---
+++ Services/IdentityService/IdentutiService.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using IdentityService.Domain.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.Infrastructure.Data
+{
+    public class AppDbContext : IdentityDbContext<AppUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Optional: You can override OnModelCreating if you want to seed roles later
+    }
+}
